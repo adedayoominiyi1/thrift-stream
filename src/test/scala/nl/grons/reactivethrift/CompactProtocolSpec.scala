d7997d8bc8d9b644d2c7ec891e3915b13ee9b083
Erik van Oosten <evanoosten@ebay.com>
2016-05-22 20:00:07 +0200

    Fixed very simple compact protocol test.

--- src/test/scala/nl/grons/reactivethrift/CompactProtocolSpec.scala
+++ src/test/scala/nl/grons/reactivethrift/CompactProtocolSpec.scala
@@ -7,14 +7,20 @@ import org.scalatest.FunSpec
 import org.scalatest.Matchers._
 import uk.co.real_logic.agrona.concurrent.UnsafeBuffer
 
-class SimpleProtocolSpec extends FunSpec {
+class CompactProtocolSpec extends FunSpec {
 
-  describe("SimpleProtocol") {
+  describe("CompactProtocol") {
     it("can decode a struct") {
       val buffer = new UnsafeBuffer(Array.ofDim[Byte](100))
       var writeIndex = 0
+      // Start of ZigZagInt field (0x45)
       buffer.putByte(writeIndex, 0x45); writeIndex += 1
-      buffer.putInt(writeIndex, 2016); writeIndex += 4  // TODO: fix byte order of writing
+      // Zig zag int with value 2016
+      val year = 2016
+      val yearAsZigZagInt = (year << 1) ^ (year >> 31)
+      buffer.putByte(writeIndex, (((yearAsZigZagInt & 0x7f) | 0x80) & 0xff).toByte); writeIndex += 1
+      buffer.putByte(writeIndex, (yearAsZigZagInt >>> 7 & 0xff).toByte); writeIndex += 1
+      // Stop field (0)
       buffer.putInt(writeIndex, 0); writeIndex += 1
 
       val bookDecoder: Decoder[Book] = CompactProtocol.structDecoder(() => new BookStructBuilder())
@@ -22,7 +28,7 @@ class SimpleProtocolSpec extends FunSpec {
       val result = bookDecoder.decode(buffer, 0)
       result shouldBe a [Decoded[_]]
       val bookResult = result.asInstanceOf[Decoded[Book]]
-      bookResult.value.year shouldBe 2016
+      bookResult.value.year shouldBe year
       bookResult.nextReadOffset shouldBe writeIndex
     }
 
